4cf04992cc717c131551105702359eb0cf2ea94f
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2026-01-19 15:04:02 +0100

    implement:contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
+function contains(obj, prop) {
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {// Check if the input is an object
+    return false;
+  }
+  return Object.prototype.hasOwnProperty.call(obj, prop); // Check if the property exists in the object
+}
 
 module.exports = contains;
+
+
