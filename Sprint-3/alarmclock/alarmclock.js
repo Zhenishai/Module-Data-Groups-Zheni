e85a4cce6c458cb356e4e1314dacb3e86b9efa79
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2026-01-26 10:52:58 +0100

    solve:alarmclock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,47 @@
-function setAlarm() {}
+let timerId = null;
+let remainingSeconds = 0;
+
+function setAlarm() {
+  //gathering input vaue
+  const input = document.getElementById("alarmSet").value;
+  remainingSeconds = Number(input);
+
+  //checking introduced data
+  if (isNaN(remainingSeconds) || remainingSeconds <= 0) {
+    return;
+  }
+
+  //Update the title immediately
+  updateDisplay(remainingSeconds);
+
+  //Clear any existing timer
+  if (timerId !== null) {
+    clearInterval(timerId);
+  }
+
+  //Start countdown
+  timerId = setInterval(() => {
+    remainingSeconds--;
+
+    updateDisplay(remainingSeconds);
+
+    //When time reaches 0
+    if (remainingSeconds <= 0) {
+      clearInterval(timerId);
+      timerId = null;
+      playAlarm();
+    }
+  }, 1000);
+}
+
+// Helper function to update the title
+function updateDisplay(seconds) {
+  const minutes = String(Math.floor(seconds / 60)).padStart(2, "0");
+  const secs = String(seconds % 60).padStart(2, "0");
+
+  document.getElementById("timeRemaining").textContent =
+    `Time Remaining: ${minutes}:${secs}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
