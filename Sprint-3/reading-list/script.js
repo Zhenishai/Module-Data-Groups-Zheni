e8eef2c1a641fa6133379680fb3541383fce04ec
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2026-01-26 11:44:54 +0100

    solve:reading-list

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -1,3 +1,37 @@
+function readingList(books) {
+  const list = document.getElementById("reading-list");
+
+  books.forEach((book) => {
+    //Create elements
+    const li = document.createElement("li");
+    const title = document.createElement("h3");
+    const author = document.createElement("p");
+    const image = document.createElement("img");
+
+    //Set text and attributes
+    title.textContent = book.title;
+    author.textContent = `by ${book.author}`;
+    image.src = book.bookCoverImage;
+    image.alt = book.title;
+
+    //Set background color depending on alreadyRead
+    if (book.alreadyRead) {
+      li.style.backgroundColor = "green";
+    } else {
+      li.style.backgroundColor = "red"; 
+    }
+
+li.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>${book.author}</p>
+      <img src="${book.bookCoverImage}" />
+    `;
+
+list.appendChild(li);
+  });
+}
+
+
 // for the tests, do not modify this array of books
 const books = [
   {
@@ -21,3 +55,4 @@ const books = [
   },
 ];
 
+readingList(books);
